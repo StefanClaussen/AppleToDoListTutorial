ffe0adb575041d53ca17924a6b8d2b4f71304530
StefenClausen <stefanclaussen@me.com>
2014-04-28 15:06:55 +0100

    Complete as of page 70

---
+++ ToDoList/STCAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  STCAddToDoItemViewController.h
+//  ToDoList
+//
+//  Created by Stefan Claussen on 28/04/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCAddToDoItemViewController : UIViewController
+
+@end
---
+++ ToDoList/STCToDoListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  STCToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Stefan Claussen on 28/04/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCToDoListTableViewController : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)sender;
+
+@end
